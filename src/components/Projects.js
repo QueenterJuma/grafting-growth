4037374edd48492abc512de2a7fe98f1a22ec8f8
QueenterJuma <jumahqueenter@gmail.com>
2024-12-23 04:43:52 +0300

    Add Projects component

---
+++ src/components/Projects.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Projects() {
+    return (
+        <section id="projects" className="projects">
+            <h2>Our Projects</h2>
+            <p>Explore the success stories of how we have positively grafted growth to the society and economic transformation.</p>
+        </section>
+    );
+}
+
+export default Projects;
