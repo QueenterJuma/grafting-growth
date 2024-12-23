3474054061830c3536b26862543054fdb3d95a5b
QueenterJuma <jumahqueenter@gmail.com>
2024-12-23 04:48:12 +0300

    Add update App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import About from './components/About';
+import Projects from './components/Projects';
+import Team from './components/Team';
+import Contact from './components/Contact';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+
+      <Routes>
+        <Route path='/' element={ <Home />} />
+        <Route path='/about' element={ <About />} />
+        <Route path='/projects' element={ <Projects/>} />
+        <Route path='/team' element={ <Team />} />
+        <Route path='/contact' element={ <Contact />} />
+      </Routes>
+
+      <Header />
+      <Footer />
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
