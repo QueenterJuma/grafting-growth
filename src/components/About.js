17691b7f43ea4129787158c424ebdbdd46787726
QueenterJuma <jumahqueenter@gmail.com>
2024-12-23 04:42:46 +0300

    Add About component

---
+++ src/components/About.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+function About() {
+    return (
+        <section id="about" className="about">
+            <h2>About Us</h2>
+            <div>
+                <h3>Missin Statement</h3>
+                <p>To nurture sustainable growth in society by empowering children, youth, women, and persons with disabilities through education, mentorship, training, and advocacy.</p>
+            </div>
+            <div>
+                <h3>Vision Statement</h3>
+                <p>A transformed society where every individual, regardless of circumstance, thrives and contributes to the collective well-being of the community.</p>
+            </div>
+            <div>
+                <h3>Aim</h3>
+                <p>To foster resilience, economic empowerment, and societal harmony through innovative initiatives that uplift the most vulnerable members of our community.</p>
+            </div>
+            <div>
+                <h3>Our Objectives</h3>
+                <p>Support for Needy Children and Orphans</p>
+                <p>Empowering Single Mothers and women in small business</p>
+                <p>Youth Empowerment</p>
+                <p>Advocacy and Support for Persons with Disabilities</p>
+            </div>
+        </section>
+    );
+}
+
+export default About;
