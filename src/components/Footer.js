4c5c16736b28287062de552791be93bab4e50bf3
QueenterJuma <jumahqueenter@gmail.com>
2024-12-23 04:42:00 +0300

    Add Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer className='footer'>
+            <p>&copy, 2024 Grafting Growth. All right reserved.</p>
+        </footer>
+    );
+}
+
+export default Footer;
