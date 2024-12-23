a63d4030e1766cf2a28b8db364accd96a0de5749
QueenterJuma <jumahqueenter@gmail.com>
2024-12-23 04:43:19 +0300

    Add Contact component

---
+++ src/components/Contact.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Contact() {
+    return (
+        <section id="contact" className="contact">
+            <h2>Contact Us</h2>
+            <p>Email: graftinggrowth@gmail.com</p>
+            <p>Phone: +254 710 976 397</p>
+        </section>
+    );
+}
+
+export default Contact;
