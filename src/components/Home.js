4151b68bc764551d8ab75ed513aa1ab2eb735778
QueenterJuma <jumahqueenter@gmail.com>
2024-12-23 04:40:45 +0300

    Add Home component

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+    return (
+        <section id="home" className="home">
+            <h1>Welcome to Grafting Growth for the jSociety</h1>
+            <p>Positively impacting the society</p>
+        </section>
+    );
+}
+
+export default Home;
